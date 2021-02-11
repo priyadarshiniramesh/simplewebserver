836cdb63b3f8bf91f5aac214ea6cd59e1cdde422
priyadarshiniramesh <rameshsonaimuthu714@gmail.com>
2021-02-11 09:17:16 +0000

    simplewebserver completed

--- multi.py
+++ multi.py
@@ -1,22 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-
-```
 from http.server import HTTPServer , BaseHTTPRequestHandler
 a = """
 <!DOCTYPE html>
@@ -53,14 +34,3 @@ server_address=('',80)
 httpd=HTTPServer(server_address,tables)
 print("WEBSERVER IS WORKING")
 httpd.serve_forever()
-
-```
-
-
-## OUTPUT:
-
-![output](./static/image/out1.jpg)
-
-## RESULT:
-
-Thus a website is designed for the simple webserver and is hosted in the URL http://priyadarshini.student.saveetha.in/ HTML code is validated. 
